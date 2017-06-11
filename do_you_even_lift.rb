e6c6ba1535604f84d01e372aaf8b13823bda46b4
Gabriel Osborne <osbornegabriel@gmail.com>
2017-06-11 13:59:21 -0500

    Write pseudocode for do you even lift

---
+++ do_you_even_lift.rb
@@ -0,0 +1,17 @@
+=begin
+ What we're going to do:
+  We are going to build a sql database that consists of two tables: Lifting & Daily Macros
+
+  The lifting table will consist of:
+    Date, Bodyweight, Squats, Bench, Press, Deadlifts, Powercleans
+  The FoodLog table will consist of:
+    Date, Total Calories, Total Fat (in g), Total Carbs (in g), total Protein (in g)
+
+  The ruby component of the programming is going to:
+  1) Create the tables if they do not exist
+  2) Allow a user to input either into the Lifting or FoodLog tables
+  3) Once the tables are selected will prompt for each value
+  4) Will execute one of two available methods for input, using the inputted values as arguments
+  ext - User can also display the contents of one or both tables. (Provide 4 options, attached to integers, for initial inquiry to user?)
+
+=end
